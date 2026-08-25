27ce59f0f95856021ffa4058de1d5a45e0a2aaf0
NCRsuite <ncr-suite@outlook.fr>
2026-08-25 20:59:49 +0200

    Align personal hourly rate metadata

---
+++ supabase/migrations/160_training_personal_hourly_rate_metadata.sql
@@ -0,0 +1,2 @@
+comment on column public.training_personal_interventions.hourly_rate_cents is
+  'Tarif horaire du suivi personnel du formateur : remuneration horaire salariee hors CA/BPF de son organisme, ou base de calcul HT pour une sous-traitance facturee via son organisme.';
