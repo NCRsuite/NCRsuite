1bdfa359f6477ed89b900fb6d1685eae995fefec
NCRsuite <ncr-suite@outlook.fr>
2026-08-27 17:15:22 +0200

    Load NCR UI 2026 people and quality layer

--- src/main.tsx
+++ src/main.tsx
@@ -21,6 +21,7 @@ import './ncrUi2026TrainingMobileFixes.css';
 import './ncrUi2026TrainingMobilePolish.css';
 import './ncrUi2026TrainingPersonalWork.css';
 import './ncrUi2026TrainingCommercialBilling.css';
+import './ncrUi2026TrainingPeopleQuality.css';
 import './trainingDossierContrastFix.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
