d1e083324f4981830980d503c5cbbbcb6a1a9f4f
NCRsuite <ncr-suite@outlook.fr>
2026-08-27 18:21:40 +0200

    Load training dashboard motion layer

--- src/main.tsx
+++ src/main.tsx
@@ -25,6 +25,7 @@ import './ncrUi2026TrainingPeopleQuality.css';
 import './ncrUi2026TrainingGovernance.css';
 import './trainingDossierContrastFix.css';
 import './ncrUi2026TrainingGovernancePolish.css';
+import './ncrUi2026TrainingDashboardMotion.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
 
