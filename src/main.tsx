d540738d1eb545970740205f62b28f602cec7450
NCRsuite <ncr-suite@outlook.fr>
2026-08-27 16:19:26 +0200

    Load training mobile polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ import './ncrUi2026TrainingWorkflowStepper.css';
 import './ncrUi2026TrainingSessions.css';
 import './ncrUi2026TrainingSessionsSummary.css';
 import './ncrUi2026TrainingMobileFixes.css';
+import './ncrUi2026TrainingMobilePolish.css';
 import './trainingDossierContrastFix.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
