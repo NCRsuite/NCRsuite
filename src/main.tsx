e3b079f56796e7e7ce43bd67bb033eadf0fda072
NCRsuite <ncr-suite@outlook.fr>
2026-08-27 22:01:35 +0200

    Load Formation sidebar polish

--- src/main.tsx
+++ src/main.tsx
@@ -32,6 +32,7 @@ import './ncrUi2026TrainingDashboardSmartPolish.css';
 import './ncrUi2026TrainingDashboardClock.css';
 import './ncrUi2026TrainingSettings.css';
 import './ncrUi2026TrainingVisualFixes.css';
+import './ncrUi2026TrainingSidebarPolish.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
 
