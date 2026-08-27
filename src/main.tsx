4b7218dcbefc3e2b4da9c74cd8a51391d2040758
NCRsuite <ncr-suite@outlook.fr>
2026-08-27 15:53:44 +0200

    Load sessions planning summary refinement

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import './ncrUi2026TrainingOperations.css';
 import './ncrUi2026TrainingSpacing.css';
 import './ncrUi2026TrainingWorkflowStepper.css';
 import './ncrUi2026TrainingSessions.css';
+import './ncrUi2026TrainingSessionsSummary.css';
 import './trainingDossierContrastFix.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
