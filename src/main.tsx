69e53e34eeef2d858ea3f3178be0beeba6f846f6
NCRsuite <ncr-suite@outlook.fr>
2026-09-03 00:03:20 +0200

    Mount Metier brand and structure enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import { ClientPortalConfigurationGuard } from './components/ClientPortalLanding
 import { ClientPortalInstallGuide } from './components/ClientPortalInstallGuide';
 import { ConnectivityStatus } from './components/ConnectivityStatus';
 import { EnterpriseOnboardingGuard } from './components/EnterpriseOnboardingGuard';
+import { MetierBrandSwitcher } from './components/MetierBrandSwitcher';
 import { MetierRuntimeBranding } from './components/MetierRuntimeBranding';
+import { MetierWorkspaceEnhancements } from './components/MetierWorkspaceEnhancements';
 import { RuntimeMonitor } from './components/RuntimeMonitor';
 import { NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED } from './config/ui2026';
 import { AuthProvider } from './contexts/AuthContext';
@@ -43,6 +45,7 @@ import './ncrUi2026WorkspaceContextPolish.css';
 import './ncrUi2026WorkspaceContextMicroPolish.css';
 import './ncrUi2026ClientPortalBranding.css';
 import './ncrUi2026MetierWhiteLabel.css';
+import './metierStructure.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
 
@@ -83,6 +86,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <PlatformAdminProvider>
           <OrganizationProvider>
             <MetierRuntimeBranding />
+            <MetierBrandSwitcher />
+            <MetierWorkspaceEnhancements />
             <RuntimeMonitor />
             <ConnectivityStatus />
             <ClientPortalConfigurationGuard>
