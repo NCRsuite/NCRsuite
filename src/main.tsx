ec80bc309ae9abe444d2dccd9e9b9d5137545892
NCRsuite <ncr-suite@outlook.fr>
2026-09-04 01:21:17 +0200

    style(beauty): load availability planning styles

--- src/main.tsx
+++ src/main.tsx
@@ -58,6 +58,7 @@ import './publicMetierCoiffureCompany.css';
 import './beautyMetierShell.css';
 import './beautySidebarNavigation.css';
 import './beautyCenterSwitcher.css';
+import './beautyAvailabilityBlocks.css';
 
 document.documentElement.setAttribute(NCR_UI_2026_DATA_ATTRIBUTE, NCR_UI_2026_ENABLED ? 'true' : 'false');
 
